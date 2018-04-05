50a12a984f83c52997738a6a092c4ab753f13bae
ReinholdLocal <efimi@me.com>
2018-04-05 13:54:13 +0200

    added Middleware GetUserFormToken JWT for proteced Methode user() and jwt:generate and .env added JWT_Sectret, added ImeTimelineController with index methode

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,10 @@ Route::post('/register', 'Auth\AuthController@register');
 Route::post('/login', 'Auth\AuthController@login');
 Route::post('/logout', 'Auth\AuthController@logout');
 // protect
-Route::get('/me', 'Auth\AuthController@user');
+// added in kernel.php Middleware jwt.auth // uses GetUserFromToken JWT class
+// group instead of goroue
+
+Route::group(['middleware' => 'jwt.auth'], function(){
+	Route::get('/me', 'Auth\AuthController@user');
+	Route::get('/timeline', 'TimelineController@index');
+});
