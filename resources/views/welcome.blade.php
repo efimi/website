f7c1e0f75e7235a6d91e82b927dadef0642cfb2e
ReinholdLocal <efimi@me.com>
2018-04-05 22:50:01 +0200

    added <meta name="csrf-token" content="{{ csrf_token() }}"> in header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,17 +4,17 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <script>
+            window.Laravel = {
+
+                'csrfToken': '{{ csrf_token() }}'
+            }
+        </script>
 
         <title>Laravel</title>
 
-        <script>
-            window.Laravel = {
-                'csrf': '{{ csrf_token() }}'
-            }
-        </script>
         <link rel="stylesheet" href="css/app.css">
-
-
     </head>
     <body>
         <div id="app">
